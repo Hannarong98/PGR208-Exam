b73682aeeca33d2d5c50fb48975b757d15c4b2de
Hannarong Klinjan <hannarong98@hotmail.com>
2020-03-22 15:45:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/foreignlands/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.foreignlands
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //private fun getPlaces(places: List<Place>)
+
+}
