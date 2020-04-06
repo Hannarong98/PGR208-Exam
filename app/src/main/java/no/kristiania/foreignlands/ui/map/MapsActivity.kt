6645a55d5b67993167f9a8fadddd4ad042d5793a
Hannarong Klinjan <hannarong98@hotmail.com>
2020-04-06 23:01:25 +0200

    Add screen 4 and its functionality

---
+++ app/src/main/java/no/kristiania/foreignlands/ui/map/MapsActivity.kt
@@ -0,0 +1,48 @@
+package no.kristiania.foreignlands.ui.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import no.kristiania.foreignlands.R
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private var lat : Double? = null
+    private var lon: Double? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        lon = intent.getDoubleExtra("long", -34.0)
+        lat = intent.getDoubleExtra("lat", -34.0)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val place = lat?.let { lon?.let { it1 -> LatLng(it1, it) } }
+        mMap.addMarker(place?.let { MarkerOptions().position(it) })
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(place))
+    }
+}
